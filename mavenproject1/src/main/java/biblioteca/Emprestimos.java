abc5f020fb41de0ee1c843fbfa374a931ebbf900
Carlos Eduardo <dev.carlosoliveira@gmail.com>
2024-10-18 20:51:55 -0300

    update project

--- mavenproject1/src/main/java/biblioteca/Emprestimos.java
+++ mavenproject1/src/main/java/biblioteca/Emprestimos.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package biblioteca;
 
-/**
- *
- * @author Cliente
- */
 public class Emprestimos {
+    private BibliUsuario usuario;
+    private BibliLivro livro;
     
 }

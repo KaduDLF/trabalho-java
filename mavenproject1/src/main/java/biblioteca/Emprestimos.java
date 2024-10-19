9415ac40228a28c0488d9cda48f067d7bca4c32c
Carlos Eduardo <dev.carlosoliveira@gmail.com>
2024-10-18 21:33:57 -0300

    Update Emprestimos.java

--- mavenproject1/src/main/java/biblioteca/Emprestimos.java
+++ mavenproject1/src/main/java/biblioteca/Emprestimos.java
@@ -3,5 +3,7 @@ package biblioteca;
 public class Emprestimos {
     private BibliUsuario usuario;
     private BibliLivro livro;
-    
+    private String dataEmpr;
+    private String datadevol;
+    private int idEmpr;
 }

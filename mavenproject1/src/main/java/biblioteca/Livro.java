b1385c6f1d28ccac6e7cf74b975e06ad15341d95
Heitormgc <heitormessias13@gmail.com>
2024-10-18 21:52:17 -0300

    criando classe livro

---
+++ mavenproject1/src/main/java/biblioteca/Livro.java
@@ -0,0 +1,15 @@
+
+package biblioteca;
+
+
+public class Livro {
+    
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+    private int exemplarDisponivel;
+    
+    public Livro()
+    
+    
+}

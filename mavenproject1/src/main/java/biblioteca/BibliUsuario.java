93a153ca4ca5ea29d45a2bc97b530cb8537e1ac3
FelGomes <felipeferreiraag0@gmail.com>
2024-10-18 22:06:16 -0300

    comentario sobre a variavel

--- mavenproject1/src/main/java/biblioteca/BibliUsuario.java
+++ mavenproject1/src/main/java/biblioteca/BibliUsuario.java
@@ -7,7 +7,7 @@ public class BibliUsuario {
     private String contato;
     private int idade;
     private int id;
-    private Emprestimos emprestimos;
+    private Emprestimos emprestimos; // vai puxar o emprestimo dentro de main para saber quem esta com o livro!
     
     
     public BibliUsuario(String nome, String sexo, String contato, int idade, int id){

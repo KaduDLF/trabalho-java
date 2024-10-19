1f08aa1d5fd7fd8844fd07bf0e03b603a225dd3d
Heitormgc <heitormessias13@gmail.com>
2024-10-18 22:22:15 -0300

    add get e set

--- mavenproject1/src/main/java/biblioteca/BibliLivro.java
+++ mavenproject1/src/main/java/biblioteca/BibliLivro.java
@@ -8,6 +8,7 @@ public class BibliLivro {
     private String autor;
     private int anoPublicacao;
     private int exemplarDisponivel;
+    private int idLivro;
     
     public BibliLivro(String titulo, String autor, int anoPublicacao, int exemplarDisponivel) {
         this.titulo = titulo;
@@ -16,6 +17,36 @@ public class BibliLivro {
         this.exemplarDisponivel = exemplarDisponivel;
     }
     
+    public String getTitulo() {
+        return titulo;
+    }
     
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
+   
+    public String getAutor(){
+       return autor;
+    }
+    
+    public void setAutor(String autor){
+        this.autor = autor;
+    }
+    
+    public int getAnoPublicacao(){
+        return anoPublicacao;
+    }
+    
+    public void setAnoPublicacao(int anoPublicacao){
+        this.anoPublicacao = anoPublicacao;
+    }
+    
+    public int getExemplarDisponivel(){
+        return exemplarDisponivel;
+    }
+    
+    public void setexEmplarDisponivel(int exemplarDisponivel){
+        this.exemplarDisponivel = exemplarDisponivel;
+    }
     
 }
